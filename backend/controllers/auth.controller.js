0a3ed96ec5521dc3919f108509e991807d27ffc1
Alok719 <alokgupta719@gmail.com>
2024-08-07 18:06:41 +0530

    useLogin hook added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -52,27 +52,26 @@ const signup = async (req, res) => {
 };
 const login = async (req, res) => {
   try {
-    const { userName, password } = req.body;
-    const user = await User.findOne({ userName });
+    const { username, password } = req.body;
+    const user = await User.findOne({ userName: username });
     const isPasswordCorrect = await bcrypt.compare(
       password,
       user?.password || ""
     );
     if (!user || !isPasswordCorrect) {
-      return res.status(400).json({ error: `Invalid username or password` });
+      return res.status(400).json({ error: "Invalid username or password" });
     }
-
     generateTokenAndSetCookie(user._id, res);
 
     res.status(200).json({
       _id: user._id,
       fullName: user.fullName,
-      userName: user.userName,
+      username: user.userName,
       profile: user.profilePic,
     });
   } catch (error) {
-    console.log(`error in the login controller`, error.message);
-    res.status(400).json({ error: `internal server error` });
+    console.log("error in the login controller", error.message);
+    res.status(400).json({ error: "internal server error" });
   }
 };
 const logout = (req, res) => {
