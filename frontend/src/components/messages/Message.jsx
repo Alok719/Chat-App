7d51ae2d374c252d0f1a9fc70f767bec1bc296c3
Alok719 <alokgupta719@gmail.com>
2024-08-18 11:04:38 +0530

    search Input field of sidebar

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { useAuthContext } from "../../context/AuthContext";
 import useConverstation from "../../zustand/useConversation";
+import { extractTime } from "../../utils/extractTime";
 const Message = ({ message }) => {
   const { authUser } = useAuthContext();
   const { selectedConversation } = useConverstation();
   const fromMe = message.senderId === authUser._id;
+  const formattedTime = extractTime(message.createdAt);
   const chatClassName = fromMe ? "chat-end" : "chat-start";
   const profilePic = fromMe
     ? authUser.profile
@@ -12,7 +14,7 @@ const Message = ({ message }) => {
   const bubbleBgColor = fromMe ? "bg-blue-500" : " ";
   return (
     <div className={`chat ${chatClassName}`}>
-      <div className={`chat-bubble text-white  ${bubbleBgColor}`}>
+      <div className={`chat-bubble text-white  ${bubbleBgColor} pd-2`}>
         {message.message}
       </div>
       <div className="chat-image-avatar">
@@ -21,7 +23,7 @@ const Message = ({ message }) => {
         </div>
       </div>
       <div className={`chat-footer opacity-50 text-xs flex gap-1 items-center`}>
-        {message.createdAt}
+        {formattedTime}
       </div>
     </div>
   );
--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,12 +1,39 @@
+import { useState } from "react";
 import { IoSearchSharp } from "react-icons/io5";
+import useConverstation from "../../zustand/useConversation";
+import useGetConversations from "../../hooks/useGetConversations";
+import toast from "react-hot-toast";
 
 const SearchInput = () => {
+  const [search, setSearch] = useState("");
+  const { setSelectedConversation } = useConverstation();
+  const { conversations } = useGetConversations();
+
+  const hadleSubmit = (e) => {
+    e.preventDefault();
+    if (!search) return;
+    if (search.length < 3) {
+      return toast.error(`Search input should be at least 3 chararcter long `);
+    }
+    const conversation = conversations.find((user) =>
+      user.fullName.toLowerCase().includes(search.toLowerCase())
+    );
+
+    if (conversation) {
+      setSelectedConversation(conversation);
+      setSearch("");
+    } else {
+      toast.error(`No such user find`);
+    }
+  };
   return (
-    <form className="flex items-center gap-2">
+    <form onSubmit={hadleSubmit} className="flex items-center gap-2">
       <input
         type="text"
         placeholder="Search…"
         className="input input-bordered rounded-full"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
       <button type="submit" className="btn btn-circle bg-sky-500 text-white">
         <IoSearchSharp className="w-6 h-6 outline-none" />
